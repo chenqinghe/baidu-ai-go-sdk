4183affaaba6d499607c0ab754666d9a9d8796a8
chenqinghe <chenqh721@foxmail.com>
2017-07-15 23:39:02 +0800

    finish general OCR

--- ocr/general.go
+++ ocr/general.go
@@ -6,13 +6,35 @@ import (
 	"github.com/imroc/req"
 )
 
-const OCR_GENERAL_BASIC_URL string = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
+const (
+	OCR_GENERAL_BASIC_URL         string = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
+	OCR_GENERAL_WITH_LOCATION_URL        = "https://aip.baidubce.com/rest/2.0/ocr/v1/general"
+	OCR_GENERAL_ENHANCED_URL             = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_enhanced"
+)
 
 var defaultGeneralBasicParams = map[string]string{
 	"image":            "",        //图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
 	"language_type":    "CHN_ENG", //识别语言类型，默认为CHN_ENG。可选值包括： - CHN_ENG：中英文混合； - ENG：英文； - POR：葡萄牙语； - FRE：法语； - GER：德语； - ITA：意大利语； - SPA：西班牙语； - RUS：俄语； - JAP：日语
 	"detect_direction": "false",   //是否检测图像朝向，默认不检测，即：false。朝向是指输入图像是正常方向、逆时针旋转90/180/270度。可选值包括: - true：检测朝向； - false：不检测朝向。
 	"detect_language":  "false",   //是否检测语言，默认不检测。当前支持（中文、英语、日语、韩语）
+}
+
+var defaultGeneralWithLocationParams = map[string]string{
+	"image":                 "",        //图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
+	"recognize_granularity": "big",     //是否定位单字符位置，big：不定位单字符位置，默认值；small：定位单字符位置
+	"language_type":         "CHN_ENG", //识别语言类型，默认为CHN_ENG。可选值包括： - CHN_ENG：中英文混合； - ENG：英文； - POR：葡萄牙语； - FRE：法语； - GER：德语； - ITA：意大利语； - SPA：西班牙语； - RUS：俄语； - JAP：日语
+	"detect_direction":      "false",   //是否检测图像朝向，默认不检测，即：false。朝向是指输入图像是正常方向、逆时针旋转90/180/270度。可选值包括:- true：检测朝向； - false：不检测朝向
+	"detect_language":       "false",   //是否检测语言，默认不检测。当前支持（中文、英语、日语、韩语）
+	"vertexes_location":     "false",   //是否返回文字外接多边形顶点位置，不支持单字位置。默认为false
+	"probability":           "false",   //是否返回识别结果中每一行的置信度
+}
+
+var defaultDeneralEnhancedParams = map[string]string{
+	"image":            "",        //图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
+	"language_type":    "CHN_ENG", //识别语言类型，默认为CHN_ENG。可选值包括： - CHN_ENG：中英文混合； - ENG：英文； - POR：葡萄牙语； - FRE：法语； - GER：德语； - ITA：意大利语； - SPA：西班牙语； - RUS：俄语； - JAP：日语
+	"detect_direction": "false",   //是否检测图像朝向，默认不检测，即：false。朝向是指输入图像是正常方向、逆时针旋转90/180/270度。可选值包括:- true：检测朝向； - false：不检测朝向
+	"detect_language":  "false",   //是否检测语言，默认不检测。当前支持（中文、英语、日语、韩语）
+	"probability":      "false",   //是否返回识别结果中每一行的置信度
 }
 
 type OCRClient struct {
@@ -33,14 +55,14 @@ func (oc *OCRClient) GeneralRecognizeBasic(img []byte, conf map[string]string) (
 		return []byte{}, err
 	}
 	encodedImgStr := base64.StdEncoding.EncodeToString(img)
-	defaultGeneralBasicParams["image"] = encodedImgStr
-	for key, _ := range defaultGeneralBasicParams {
-		if v, ok := conf[key]; ok {
-			defaultGeneralBasicParams[key] = v
+	conf["image"] = encodedImgStr
+	for key, value := range defaultGeneralBasicParams {
+		if _, ok := conf[key]; !ok {
+			conf[key] = value
 		}
 	}
 	var url string = OCR_GENERAL_BASIC_URL + "?access_token=" + oc.AccessToken
-	resp, err := req.Post(url, req.Param(defaultGeneralBasicParams), req.Header{
+	resp, err := req.Post(url, req.Param(conf), req.Header{
 		"Content-Type": "application/x-www-form-urlencoded",
 	})
 	if err != nil {
@@ -54,11 +76,51 @@ func (oc *OCRClient) GeneralRecognizeBasic(img []byte, conf map[string]string) (
 }
 
 //GeneralRecognizeWithLocation 通用文字识别（含位置信息）
-func (oc *OCRClient) GeneralRecognizeWithLocation(img []byte, conf map[string]string) {
+func (oc *OCRClient) GeneralRecognizeWithLocation(img []byte, conf map[string]string) (rs []byte, err error) {
+	if err = oc.Auth(); err != nil {
+		return
+	}
+
+	encodedImgStr := base64.StdEncoding.EncodeToString(img)
+	conf["image"] = encodedImgStr
+	for key, value := range defaultGeneralWithLocationParams {
+		if _, ok := conf[key]; !ok {
+			conf[key] = value
+		}
+	}
+
+	var url string = OCR_GENERAL_WITH_LOCATION_URL + "?access_token=" + oc.AccessToken
+	resp, err := req.Post(url, req.Param(conf), req.Header{"Content-Type": "application/x-www-form-urlencoded"})
+	if err != nil {
+		return
+	}
+	rs, err = resp.ToBytes()
+	return
 
 }
 
 //GeneralRecognizeEnhanced 通用文字识别（含生僻字）
-func (oc *OCRClient) GeneralRecognizeEnhanced() {
+func (oc *OCRClient) GeneralRecognizeEnhanced(img []byte, conf map[string]string) (rs []byte, err error) {
+	if err = oc.Auth(); err != nil {
+		return
+	}
+
+	encodedImgStr := base64.StdEncoding.EncodeToString(img)
+	conf["image"] = encodedImgStr
+
+	for key, value := range defaultDeneralEnhancedParams {
+		if _, ok := conf[key]; !ok {
+			conf[key] = value
+		}
+
+	}
+
+	url := OCR_GENERAL_ENHANCED_URL + "?access_token=" + oc.AccessToken
+	resp, err := req.Post(url, req.Param(conf), req.Header{"Content-Type": "application/x-www-form-urlencoded"})
+	if err != nil {
+		return
+	}
+	rs, err = resp.ToBytes()
+	return
 
 }
--- ocr/general_test.go
+++ ocr/general_test.go
@@ -7,19 +7,16 @@ import (
 	"testing"
 )
 
-var apikey string = "nKesFsdfgdOqHYzKFMzVcvdfX4cfDU"
-var secretkey string = "2xTn6TGucUNa6YfgfdUZoDOMeZWqYsKpop1n"
+var apikey string = "nKesFdOqHYzKFMzVasdcvX4cfDU"
+var secretkey string = "2xTn6TGucdffssUNa6YUZoDOMeZWqYsKpop1n"
+
+var client *OCRClient = NewOCRClient(apikey, secretkey)
 
 func TestGeneralRecognizeBasic(t *testing.T) {
-	f, err := os.OpenFile("ocr.jpg", os.O_RDONLY, 0777)
+	img, err := openfile("ocr.jpg")
 	if err != nil {
 		t.Fatal(err)
 	}
-	img, err := ioutil.ReadAll(f)
-	if err != nil {
-		t.Fatal(err)
-	}
-	client := NewOCRClient(apikey, secretkey)
 	var conf map[string]string = make(map[string]string)
 	bts, err := client.GeneralRecognizeBasic(img, conf)
 	if err != nil {
@@ -27,4 +24,47 @@ func TestGeneralRecognizeBasic(t *testing.T) {
 	}
 	fmt.Println(string(bts))
 	t.Log("testing passed.")
+}
+
+func TestOCRClient_GeneralRecognizeWithLocation(t *testing.T) {
+	img, err := openfile("ocr.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var conf map[string]string = make(map[string]string)
+	bts, err := client.GeneralRecognizeWithLocation(img, conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(string(bts))
+	t.Log("testing passed.")
+}
+
+func TestOCRClient_GeneralRecognizeEnhanced(t *testing.T) {
+
+	img, err := openfile("ocr.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var conf map[string]string = make(map[string]string)
+	bts, err := client.GeneralRecognizeEnhanced(img, conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(string(bts))
+	t.Log("testing passed.")
+
+}
+
+func openfile(filename string) ([]byte, error) {
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0777)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
 }
